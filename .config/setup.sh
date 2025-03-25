1a29cc24746e26ef46e8608ee285d78aff799188
Philipp Hanemann <p.hanemann@enbw.com>
2025-03-25 08:09:04 +0100

    convenience setup script

---
+++ .config/setup.sh
@@ -0,0 +1,38 @@
+# setting up things
+sudo apt update
+
+# setup python
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.12-dev
+
+# pipx
+sudo apt install pipx -y
+pipx ensurepath
+
+# install cmake and gettext
+sudo apt install cmake gettext ripgrep fd-find fzf -y
+
+mkdir dev/
+# install neovim
+git clone git@github.com:neovim/neovim.git ~/dev
+cd ~/dev/neovim
+git checkout stable
+
+# install under home not default
+rm -r build/ # clear the CMake cache
+make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
+make install
+export PATH="$HOME/neovim/bin:$PATH"
+
+## setup lazy vim 
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+
+
+# install ranger and 
+pipx install ranger-fm
+
+
