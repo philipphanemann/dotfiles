0018b89b498a066926f84411cb4bde96fe872e6a
Philipp Hanemann <p.hanemann@enbw.com>
2025-03-27 08:08:37 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo apt update
 # setup python
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install python3.12-dev
+sudo apt-get install python3.12-dev -y
 
 # pipx
 sudo apt install pipx -y
@@ -14,9 +14,11 @@ pipx ensurepath
 # install cmake and gettext
 sudo apt install cmake gettext ripgrep fd-find fzf npm unzip -y
 
-mkdir dev/
+mkdir $HOME/dev/
+mkdir $HOME/downloads/
+
 # install neovim
-git clone git@github.com:neovim/neovim.git ~/dev
+git clone https://github.com/neovim/neovim.git $HOME/dev
 cd ~/dev/neovim
 git checkout stable
 
@@ -27,10 +29,12 @@ make install
 export PATH="$HOME/neovim/bin:$PATH"
 
 ## setup lazy vim
+cd $HOME/downloads/
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
+cd $HOME
 
 # install ranger and tldr
 pipx install ranger-fm tldr
