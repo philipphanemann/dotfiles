77839694ffd33910832a63598ebd4da615186170
Philipp Hanemann <p.hanemann@enbw.com>
2025-05-23 09:02:58 +0200

    lua help

--- .config/lazyvim_config/init.lua
+++ .config/lazyvim_config/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- clear all
+--rm -rf ~/.config/nvim && rm -rf ~/.local/share/nvim && rm -rf ~/.local/state/nvim && rm -rf ~/.cache/nvim
